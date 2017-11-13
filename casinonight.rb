4dde65ae2b907c85d0760cef9260666169fd9693
Matt Berz <mberzovich@gmail.com>
2017-11-13 10:34:22 -0700

    added high low game

--- casinonight.rb
+++ casinonight.rb
@@ -26,17 +26,33 @@
 
 
 require_relative 'wallet'
+require_relative 'high_low'
 require 'pry'
 require 'colorize'
 
 class Casino
+  @options = ["High / Low", "Exit"]
+
+  attr_accessor :player
+
   def initialize
     puts ''
     puts 'Welcome to the ruby casino'
     @player = Player.new
-    binding.pry
-
+    menu
   end
+
+  def menu
+    @options.each_with_index { |opt, i| puts "#{i + 1}) #{opt}"}
+    choice = gets.to_i - 1
+    case choice
+      when 0
+      when 1
+        puts "Come Again!"
+        exit
+      else puts "Invalid Choice"
+        menu
+
 end
 
 class Player
